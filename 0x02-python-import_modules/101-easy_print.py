93ce0a192a6052d34fecd060b60d153af17d5647
Abdelbar Amejjoud <abbelabbel9@gmail.com>
2023-09-08 01:56:04 +0100

    Update

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,2 +1,2 @@
-#!/usr/bin/python3 # AUTHOR : ABDELBAR AD
+#!/usr/bin/python3                                                            # AUTHOR : ABDELBAR AD
 __import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
