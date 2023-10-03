60f00343786f91baade03adb5df6a723af7c0a91
Abdelbar Amejjoud <abbelabbel9@gmail.com>
2023-10-03 01:37:18 +0100

    Update 101

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""ABDELBAR AD"""
-"""Solves the N-queens puzzle.
+"""ABDELBAR AD""" """Solves the N-queens puzzle.
 
 Determines all possible solutions to placing N
 N non-attacking queens on an NxN chessboard.
